c1f944ee1adff058affd887b37eca185e32ee3aa
ncnuovecreazioni <ncnuovecreazioni@gmail.com>
2026-09-18 07:22:08 +0200

    Link affiliate transparency from product layer

--- product-layer.js
+++ product-layer.js
@@ -109,7 +109,7 @@
     var bar=document.createElement('div');
     bar.id='px-product-homebar';
     bar.className='px-product-homebar';
-    bar.innerHTML='<a href="/soluzioni.html">Soluzioni</a><a href="/faq.html">FAQ</a><a href="/report-pro.html">Report PRO</a><a href="/coach.html">AI Coach</a><a href="/affiliate.html">Partner</a>';
+    bar.innerHTML='<a href="/soluzioni.html">Soluzioni</a><a href="/faq.html">FAQ</a><a href="/report-pro.html">Report PRO</a><a href="/coach.html">AI Coach</a><a href="/affiliate.html">Partner</a><a href="/affiliate-disclosure.html">Trasparenza</a>';
     hero.appendChild(bar);
   }
 
